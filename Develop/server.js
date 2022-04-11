2af16afc86069db21754c03421aee38b353fb203
Theo Ward <theoward9@gmail.com>
2022-04-11 18:42:03 -0500

    added helper info to server

--- Develop/server.js
+++ Develop/server.js
@@ -3,11 +3,14 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const util = require('util');
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 const api = require('./routes/api');
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 //gives access to files in public folder for hosting
 app.use(express.static('public'));
 
